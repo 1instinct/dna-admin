4da375c3c82ffb97b07777076a0006b3b7e5e7a2
Aaron Smulktis <aaronsmulktis@gmail.com>
2019-07-10 17:43:54 -0700

    add TWILIO to env.example, add static_content extension, tweak README, redirect root to login, redirect after login to adminy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_153324) do
+ActiveRecord::Schema.define(version: 2019_07_10_194432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
