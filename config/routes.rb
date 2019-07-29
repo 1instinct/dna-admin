fc50532aab0cf7bfc6c7f95982597a61db4eba45
Aaron Smulktis <aaronsmulktis@gmail.com>
2019-07-29 15:26:11 -0700

    remove unnecessary user_sessions_controller, and cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  # This line mounts Spree's routes at the root of your application.
-  # This means, any requests to URLs such as /products, will go to
-  # Spree::ProductsController.
-  # If you would like to change where this engine is mounted, simply change the
-  # :at option to something different.
-  #
-  # We ask that you don't use the :as option here, as Spree relies on it being
-  # the default of "spree".
+
   mount Spree::Core::Engine, at: '/'
-  
+
 end
 
 Spree::Core::Engine.add_routes do
@@ -19,7 +12,7 @@ Spree::Core::Engine.add_routes do
 
     get "/messages" => "messages#index"
     get "/messages/support" => "messages#message_support"
-    
+
   end
 
 end
