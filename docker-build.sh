9713412d0d87f8b5dce9e00fe4329c08b1e427b6
David Dombrowsky <davek@6thstreetradio.org>
2021-03-10 13:14:31 -0500

    avoid prompting user when building the containers

--- docker-build.sh
+++ docker-build.sh
@@ -8,7 +8,8 @@ set -e
 docker-compose build
 docker-compose up -d
 
-docker-compose exec web rails db:create db:schema:load db:migrate db:seed
+docker-compose exec web rails db:create db:schema:load db:migrate
+docker-compose exec -e ADMIN_EMAIL=spree@example.com -e ADMIN_PASSWORD=spree123 web rails db:seed
 
 docker-compose exec web rails spree_sample:load
 docker-compose restart
