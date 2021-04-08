b5ca290f13c54d32d69b79e6391e0e51eb8129a5
David Dombrowsky <davek@6thstreetradio.org>
2021-04-08 04:02:09 +0000

    fixed bucket name env, added k8 deploy script

--- k8-deploy.sh
+++ k8-deploy.sh
@@ -8,16 +8,9 @@ set -ex
 # or another registry server.
 
 docker-compose build
-
-# wait for web to build assets
-# FIXME: this doesn't work
-docker-compose up
-sleep 5 ; docker-compose stop
-
-docker commit dna-admin_web_1 dna-admin_web:0.1
-docker tag dna-admin_web:0.1 dna-admin:0.1
+docker tag dna-admin_web dna-admin:0.1
 docker save dna-admin > dna-admin.tar
 microk8s ctr image import dna-admin.tar
 
-microk8s kubectl delete deployment.apps/dna-admin || :
-microk8s kubectl apply -f dna-k8.yaml
+kubectl delete deployment.apps/dna-admin || :
+kubectl apply -f dna-k8.yaml
