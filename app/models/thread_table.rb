9b0e67e44f479540971508d41c5738e9bfdbcc62
mayankg-protonshub <mayank.g@protonshub.com>
2021-07-20 19:02:03 +0530

    Add thread table

--- app/models/thread_table.rb
+++ app/models/thread_table.rb
@@ -1,6 +1,2 @@
-class ThreadTable < Spree::Base
-  has_one :live_stream, dependent: :destroy
-  has_one :actor, dependent: :destroy
-  has_many :messages, dependent: :destroy
-  self.whitelisted_ransackable_attributes = %w[id]
+class ThreadTable < ApplicationRecord
 end
