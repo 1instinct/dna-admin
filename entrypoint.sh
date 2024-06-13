502729da1f544ebccbb3f1072c8c015d09d8ab26
smokey <aaronsmulktis@gmail.com>
2024-06-12 20:59:14 -0700

    regenerated Gemfile.lock

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,5 @@ rm -f /myapp/tmp/pids/server.pid
 
 echo RAILS_ENV=$RAILS_ENV
 
-<<<<<<< HEAD
-=======
-if [ ! -e assets_precompiled ] ; then
-    rails assets:precompile
-    touch assets_precompiled
-    exit 0
-fi
-
->>>>>>> fa55c3c (compile assets once on container start)
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
