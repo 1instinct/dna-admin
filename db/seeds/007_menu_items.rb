bf30088d193539f72c7981bebc1b2509b2782bc6
Jonathan Schimpf <Jonathan.schimpf@gmail.com>
2026-01-10 12:40:59 -0500

    Replace lorem ipsum in static pages and menu items
    - Update static pages with realistic titles (About Us, Contact, FAQ, etc.)
    - Replace menu items with proper navigation (Home, Shop, New Arrivals, etc.)
    - Reduce menu items from 50 to 8 focused navigation links

--- db/seeds/007_menu_items.rb
+++ db/seeds/007_menu_items.rb
@@ -1,15 +1,27 @@
 menu_location_ids = MenuLocation.pluck(:id)
-(1..50).each do |i|
+
+menu_items = [
+  { name: 'Home', url: '/', item_class: 'nav-home' },
+  { name: 'Shop', url: '/products', item_class: 'nav-shop' },
+  { name: 'New Arrivals', url: '/t/new-arrivals', item_class: 'nav-new' },
+  { name: 'Sale', url: '/t/sale', item_class: 'nav-sale' },
+  { name: 'Collections', url: '/t/collections', item_class: 'nav-collections' },
+  { name: 'Live Shopping', url: '/live-streams', item_class: 'nav-live' },
+  { name: 'About Us', url: '/pages/about-us', item_class: 'nav-about' },
+  { name: 'Contact', url: '/pages/contact', item_class: 'nav-contact' }
+]
+
+menu_items.each_with_index do |item_data, i|
   MenuItem.create!(
-    name: Faker::Lorem.word.capitalize,
-    url: Faker::Internet.url,
-    item_class: Faker::Lorem.word,
-    item_id: Faker::Number.unique.number(digits: 5).to_s,
-    item_target: ['_blank', '_self'].sample,
-    parent_id: [nil, (1..10).to_a.sample].sample,
+    name: item_data[:name],
+    url: item_data[:url],
+    item_class: item_data[:item_class],
+    item_id: (i + 1).to_s,
+    item_target: '_self',
+    parent_id: nil,
     position: i,
-    is_visible: [true, false].sample,
-    menu_location_id: menu_location_ids.sample, # Reference an existing MenuLocation ID
+    is_visible: true,
+    menu_location_id: menu_location_ids.first,
     created_at: Time.now,
     updated_at: Time.now
   )
